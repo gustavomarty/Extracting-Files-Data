9203efef7df393c097cb0f9d1d449f0868951700
martysroka <martysroka@bne.com.br>
2020-06-20 16:45:12 -0300

    refact

--- ExtractingFilesData/OCR.cs
+++ ExtractingFilesData/OCR.cs
@@ -7,27 +7,19 @@ namespace ExtractingFilesData
     {
 		public static string ExtractContent(string imgPath)
 		{
-			try
+			using (var engine = new TesseractEngine(@"tessdata", "eng", EngineMode.Default))
 			{
-				using (var engine = new TesseractEngine(@"tessdata", "eng", EngineMode.Default))
+				using (var img = Pix.LoadFromFile(imgPath))
 				{
-					using (var img = Pix.LoadFromFile(imgPath))
+					using (var page = engine.Process(img))
 					{
-						using (var page = engine.Process(img))
-						{
-							var text = page.GetText();
+						var text = page.GetText();
 
-							Console.WriteLine($"score -> {page.GetMeanConfidence()}");
+						Console.WriteLine($"score -> {page.GetMeanConfidence()}");
 
-							return text;
-						}
+						return text;
 					}
 				}
-			}
-			catch (Exception ex)
-			{
-				Console.WriteLine($"Error: {ex.Message}");
-				return string.Empty;
 			}
 		}
 	}
